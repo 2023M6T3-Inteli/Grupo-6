bfb449fff33431c6525507c108ee96379ac30812
MateusGCN <mateus.neves@sou.inteli.edu.br>
2023-05-23 19:37:17 -0300

    feat: user/post request

--- codigo/frontend/src/lib/services/service_post.dart
+++ codigo/frontend/src/lib/services/service_post.dart
@@ -1,16 +1,18 @@
 import 'dart:convert';
-
 import 'package:http/http.dart' as http;
 
 const String baseUrl = "http://localhost:3000";
 
 void main() {
-  getAllPosts();
+  getAllPosts().then((data) {
+    print(data);
+  }).catchError((error) {
+    print('Erro: $error');
+  });
 }
 
 getAllPosts() async {
   var response = await http.get(Uri.parse("$baseUrl/post"));
-  // var json = jsonDecode(response.body);
-  print(response.body);
-  return response.body;
+  var json = jsonDecode(response.body);
+  return json;
 }
---
+++ codigo/frontend/src/lib/services/service_user.dart
@@ -0,0 +1,18 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+
+const String baseUrl = "http://localhost:3000";
+
+void main() {
+  getAllPosts().then((data) {
+    print(data);
+  }).catchError((error) {
+    print('Erro: $error');
+  });
+}
+
+getAllPosts() async {
+  var response = await http.get(Uri.parse("$baseUrl/user"));
+  var json = jsonDecode(response.body);
+  return json;
+}
