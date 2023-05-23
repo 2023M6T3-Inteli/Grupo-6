f3043a826d790ee02a6dc2e06e0f4dd51830eacd
MateusGCN <mateus.neves@sou.inteli.edu.br>
2023-05-23 16:41:40 -0300

    feat: try and catch

--- codigo/backend/src/user/user.service.ts
+++ codigo/backend/src/user/user.service.ts
@@ -1,62 +1,139 @@
-import { Injectable } from '@nestjs/common';
+//@ts-nocheck
+import { BadRequestException, Injectable, InternalServerErrorException } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
-import { UpdateUserDto } from './dto/update-user.dto';
 import { PrismaService } from '../prisma.service';
+import e from 'express';
 
 @Injectable()
 export class UserService {
   constructor(private prisma: PrismaService) {}
-
+  
   async create(data: CreateUserDto) {
-    const newUser = await this.prisma.user.create({
-      data: {
-        name: data.name,
-        role: data.role,
-        about_me: data.about_me,
-        email: data.email,
-        password: data.password,
-        badges: data.badges,
-        projects: data.projects
+    const userExist = await this.prisma.user.findMany({
+      where: {
+        email: data.email
       }
-    });
+    })
+
+    if (userExist){
+      throw new BadRequestException('Usuário com esse e-mail já existe no banco de dados.')
+      
+    };
+
+    try{
+      const newUser = await this.prisma.user.create({
+        data: {
+          name: data.name,
+          role: data.role,
+          about_me: data.about_me,
+          email: data.email,
+          password: data.password,
+          badges: data.badges,
+          posts: data.posts,
+          projects: data.projects
+        }
+      });
+    }
+
+    catch (error){
+      console.log(error);
+      throw new InternalServerErrorException('Something bad happened: ', error)
+    }
   }
 
   async findAll() {
-    const users = await this.prisma.user.findMany({})
-
-    return users
+    try{
+      const users = await this.prisma.user.findMany({})
+      return users
+    }
+    catch(error) {
+      console.log(error)
+      throw new InternalServerErrorException('Something bad happened: ', error)
+    }
   }
 
   async findOne(id: string) {
-    const user = await this.prisma.user.findUnique({
-      where: { id: id}
+    const userExist = await this.prisma.user.findUnique({
+      where: {
+        id: id
+      }
+    })
+
+    if (!userExist){
+      console.log("User not found")
+      throw new BadRequestException('Something bad happened: User not found')
+    }
+
+    try{
+      const user = await this.prisma.user.findUnique({
+        where: { id: id}
       });
-      
-      return user
+        return user
+    }
+    catch(error) {
+      console.log(error)
+      throw new InternalServerErrorException('Something bad happened: ', error)
+    }
   }
   
   async update(id: string, data: CreateUserDto) {
-    const user = await this.prisma.user.update({
-      where: {id: id},
-      data: {
-        name: data.name,
-        role: data.role,
-        about_me: data.about_me,
-        email: data.email,
-        password: data.password,
-        badges: data.badges,
-        projects: data.projects
+    const userExist = await this.prisma.user.findUnique({
+      where: {
+        id: id
       }
-    });
+    })
     
-    return user
+    if (!userExist){
+      console.log("User not found")
+      throw new BadRequestException('Something bad happened: User not found')
+    }
+
+    try{
+      const user = await this.prisma.user.update({
+        where: {id: id},
+        data: {
+          name: data.name,
+          role: data.role,
+          about_me: data.about_me,
+          email: data.email,
+          password: data.password,
+          badges: data.badges,
+          posts: data.posts,
+          projects: data.projects
+        }
+      });
+      
+      return user
+    }
+    catch(error) {
+      console.log(error)
+      throw new InternalServerErrorException('Something bad happened: ', error)
+    }
   }
 
   async delete(id: string) {
-    const user = await this.prisma.user.delete({
-      where: {id: id}
-    });
+    const userExist = await this.prisma.user.findUnique({
+      where: {
+        id: id
+      }
+    })
 
-    return user
+    if (!userExist){
+      console.log("User not found")
+      throw new BadRequestException('Something bad happened: User not found')
+    }
+
+    try{
+      const user = await this.prisma.user.delete({
+        where: {id: id}
+      });
+
+      return user
+    }
+
+    catch(error) {
+      console.log(error)
+      throw new InternalServerErrorException('Something bad happened: ', error)
+    }
   }
 }
