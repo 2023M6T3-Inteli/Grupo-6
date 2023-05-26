fc4682bbf2c3c6dade3fba24f1602e624dc50b53
vict0rcarvalh0 <victordecarvalho342@gmail.com>
2023-05-26 00:50:13 -0300

    feat: user name and role integration

--- codigo/frontend/src/lib/services/service_post.dart
+++ codigo/frontend/src/lib/services/service_post.dart
@@ -2,9 +2,9 @@ import 'dart:async';
 import 'dart:convert';
 import 'package:http/http.dart' as http;
 
-
 void main() {
-  sendPost("kinho3","teste do post","teste","675fb91e-eaf3-4611-8395-786ab81abdb0");
+  // sendPost("kinho3","teste do post","teste","675fb91e-eaf3-4611-8395-786ab81abdb0");
+  getAllPosts();
 }
 
 const String baseUrl = "http://localhost:3000";
@@ -14,9 +14,11 @@ Future<List<dynamic>> getAllPosts() async {
     var response = await http.get(Uri.parse("$baseUrl/post"));
     if (response.statusCode == 200) {
       var jsonData = jsonDecode(response.body);
+      // print(jsonData);
       return List<dynamic>.from(jsonData);
     } else {
-      throw Exception("Failed to load posts. Status code: ${response.statusCode}");
+      throw Exception(
+          "Failed to load posts. Status code: ${response.statusCode}");
     }
   } catch (e) {
     throw Exception("Failed to load posts: $e");
@@ -30,16 +32,16 @@ Future<Map<String, dynamic>> getPost(String id) async {
       var jsonData = jsonDecode(response.body);
       return jsonData as Map<String, dynamic>;
     } else {
-      throw Exception("Failed to load post. Status code: ${response.statusCode}");
+      throw Exception(
+          "Failed to load post. Status code: ${response.statusCode}");
     }
   } catch (e) {
     throw Exception("Failed to load post: $e");
   }
 }
 
-
-
-Future<void> sendPost(String title, String description, String category, String creator) async {
+Future<void> sendPost(
+    String title, String description, String category, String creator) async {
   try {
     var url = Uri.parse("$baseUrl/post");
     var headers = {'Content-Type': 'application/json'};
@@ -55,13 +57,10 @@ Future<void> sendPost(String title, String description, String category, String 
     if (response.statusCode == 200 || response.statusCode == 201) {
       print("Post enviado com sucesso!");
     } else {
-      throw Exception("Failed to send post. Status code: ${response.statusCode}");
+      throw Exception(
+          "Failed to send post. Status code: ${response.statusCode}");
     }
   } catch (e) {
     throw Exception("Failed to send post: $e");
   }
 }
-
-
-
-
