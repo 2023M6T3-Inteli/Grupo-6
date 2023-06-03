119d1c488cc191d77707a96eac4395cec575b7c8
MateusGCN <mateus.neves@sou.inteli.edu.br>
2023-06-03 16:05:52 -0300

    feat: user created in old db if not exist

--- codigo/backend/src/login/login.service.ts
+++ codigo/backend/src/login/login.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@nestjs/common';
+import axios from 'axios';
 import { PrismaService } from 'src/prisma.service';
 import { FindLoginDto } from './dto/find-login.dto';
 
@@ -11,6 +12,41 @@ export class LoginService {
     const login = await this.prisma.user.findFirst({
       where: { email: FindLoginDto.email, password: FindLoginDto.password }
     });
+
+    
+    try {
+      const response = await axios({
+        method: 'get',
+        url: 'http://localhost:3001/profile/'+ login.id,
+      });
+
+
+    } catch (error) {
+      if (error.response && error.response.status === 404) {
+        var data = JSON.stringify({
+          "id_profile": login.id,
+          "name": login.name,
+          "email": login.email
+        });
+        
+        var config = {
+          method: 'post',
+          url: 'http://localhost:3001/profile',
+          headers: { 
+            'Content-Type': 'application/json'
+          },
+          data : data
+        };
+        
+        const creatUser = await axios(config)
+        .then(function (response) {
+          console.log(JSON.stringify(response.data));
+        })
+        .catch(function (error) {
+          console.log(error);
+        });
+      }
+    }
     return login.id;
   }
 }
