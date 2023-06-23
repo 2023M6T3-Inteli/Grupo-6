6a83f82e2f239bd348f9f717402ebcf84f598714
vict0rcarvalh0 <victordecarvalho342@gmail.com>
2023-06-23 13:54:11 -0300

    feat: creating user unit tests

--- codigo/backend/src/user/entities/user.entity.ts
+++ codigo/backend/src/user/entities/user.entity.ts
@@ -1,1 +1,22 @@
-export class User {}
+import { CreateBadgeDto } from "src/badge/dto/create-badge.dto";
+import { CreatePostDto } from "src/post/dto/create-post.dto";
+
+export class User {
+    id: string;
+    name: string;
+    role: string;
+    about_me: string;
+    email: string;
+    password: string;
+    createdAt: Date;
+    updatedAt: Date;
+    badges: CreateBadgeDto;
+    posts: CreatePostDto;
+    projects: string;
+    hard_skills: string;
+    soft_skills: string;
+    
+    constructor(data: Partial<User>) {
+        Object.assign(this, data);
+      }
+}
