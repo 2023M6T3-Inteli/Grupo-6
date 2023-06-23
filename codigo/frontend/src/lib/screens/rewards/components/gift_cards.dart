d1d97694fa4df14efbfe6d02740b93dd015cdffe
vict0rcarvalh0 <victordecarvalho342@gmail.com>
2023-06-23 12:22:55 -0300

    fix: adjusting card size

--- codigo/frontend/src/lib/screens/rewards/components/gift_cards.dart
+++ codigo/frontend/src/lib/screens/rewards/components/gift_cards.dart
@@ -12,114 +12,87 @@ class GiftCard extends StatelessWidget {
     required this.points,
   });
 
-  List<String> giftCardImages = [
-    'codigo/frontend/src/assets/images/image (1).png',
-    'codigo/frontend/src/assets/images/image (2).png',
-    'codigo/frontend/src/assets/images/image (3).png',
-    'codigo/frontend/src/assets/images/image (4).png',
-    'codigo/frontend/src/assets/images/image (5).png',
-  ];
-
-  List<String> giftCardTitles = [
-    'Ifood - 50',
-    'Bk - 100',
-    'Americanas - 150',
-    'Steam - 200',
-    'Espatar - 250',
-  ];
-
-  List<String> giftCardPoints = [
-    '200 points',
-    '400 points',
-    '600 points',
-    '800 points',
-    '1000 points',
-  ];
-
   @override
   Widget build(BuildContext context) {
     return Padding(
         padding: const EdgeInsets.all(10.0),
-        child: 
-          // for (var i = 0; i < 5; i++)
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: ClipRRect(
-                  borderRadius: BorderRadius.circular(15.0),
-                  child: Container(
-                    width: 400,
-                    height: 100,
-                    decoration: BoxDecoration(
-                      color: Color.fromARGB(245, 246, 247, 245),
-                      borderRadius: BorderRadius.circular(15.0),
-                      boxShadow: [
-                        BoxShadow(
-                          color: Colors.grey.withOpacity(0.75),
-                          spreadRadius: 0,
-                          blurRadius: 8,
-                          offset:
-                              const Offset(4, 4), // define a posição da sombra
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(15.0),
+                child: Container(
+                  width: 300,
+                  height: 80,
+                  decoration: BoxDecoration(
+                    color: Color.fromARGB(245, 246, 247, 245),
+                    borderRadius: BorderRadius.circular(15.0),
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.grey.withOpacity(0.75),
+                        spreadRadius: 0,
+                        blurRadius: 8,
+                        offset:
+                            const Offset(4, 4), // define a posição da sombra
+                      ),
+                    ],
+                  ),
+                  child: Row(
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.only(left: 15),
+                        child: ClipRRect(
+                          borderRadius: BorderRadius.circular(15.0),
+                          child: Image.asset(
+                            image,
+                            width: 50,
+                            height: 50,
+                          ),
                         ),
-                      ],
-                    ),
-                    child: Row(
-                      children: [
-                        Padding(
-                          padding: const EdgeInsets.only(left: 25),
-                          child: ClipRRect(
-                            borderRadius: BorderRadius.circular(15.0),
-                            child: Image.asset(
-                              image,
-                              width: 60,
-                              height: 60,
+                      ),
+                      Padding(
+                        padding: EdgeInsets.only(left: 15),
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              title,
+                              style: const TextStyle(
+                                  fontSize: 14,
+                                  fontWeight: FontWeight.bold,
+                                  color: Color.fromARGB(255, 0, 42, 88)),
+                            ),
+                            Text(
+                              points,
+                              style: const TextStyle(
+                                  fontSize: 12,
+                                  color: Color.fromARGB(255, 99, 99, 99)),
+                            ),
+                          ],
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(left: 1),
+                        child: ElevatedButton(
+                          onPressed: () {},
+                          style: ElevatedButton.styleFrom(
+                            primary: Color.fromARGB(255, 0, 42, 88),
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(10.0),
                             ),
                           ),
+                          child: const Text('Rescue'),
                         ),
-                        Padding(
-                          padding: EdgeInsets.only(left: 25),
-                          child: Column(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              Text(
-                                title,
-                                style: const TextStyle(
-                                    fontSize: 16,
-                                    fontWeight: FontWeight.bold,
-                                    color: Color.fromARGB(255, 0, 42, 88)),
-                              ),
-                              Text(
-                                points,
-                                style: const TextStyle(
-                                    fontSize: 10,
-                                    color: Color.fromARGB(255, 99, 99, 99)),
-                              ),
-                            ],
-                          ),
-                        ),
-                        Padding(
-                          padding: const EdgeInsets.only(left: 50),
-                          child: ElevatedButton(
-                            onPressed: () {},
-                            style: ElevatedButton.styleFrom(
-                              primary: Color.fromARGB(255, 0, 42, 88),
-                              shape: RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(10.0),
-                              ),
-                            ),
-                            child: const Text('Rescue'),
-                          ),
-                        )
-                      ],
-                    ),
+                      )
+                    ],
                   ),
                 ),
               ),
-            ],
-          )
-        );
+            ),
+          ],
+        ));
   }
 }
